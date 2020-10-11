7e0bf0b086d042f403c62ff8795e0a9210d7a1c5
VenkatVuddagiri <68139968+VenkatVuddagiri@users.noreply.github.com>
2020-10-11 16:43:25 +0530

    Create Main.java

---
+++ Day02/Assignment/Main.java
@@ -0,0 +1,12 @@
+//package com.employee;//optional in IDEs other than Intellij
+
+public class Employee {//Employee class
+    String name;//attributes
+    int age;
+    String city;
+    public void display(){//display function
+        System.out.println("The name of employee is "+name+".");
+        System.out.println("The age of employee is "+age+".");
+        System.out.println("The city of employee is "+city+".");
+    }
+}
