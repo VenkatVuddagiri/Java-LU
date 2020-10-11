4c769c0e8fd9f4cd8c17d3a9f87383fe0bd379e8
VenkatVuddagiri <68139968+VenkatVuddagiri@users.noreply.github.com>
2020-10-11 16:44:56 +0530

    Update Main.java
    Corrected wrong code

--- Day02/Assignment/Main.java
+++ Day02/Assignment/Main.java
@@ -1,12 +1,21 @@
-//package com.employee;//optional in IDEs other than Intellij
-
-public class Employee {//Employee class
-    String name;//attributes
-    int age;
-    String city;
-    public void display(){//display function
-        System.out.println("The name of employee is "+name+".");
-        System.out.println("The age of employee is "+age+".");
-        System.out.println("The city of employee is "+city+".");
+package com.employee;
+//create employee class
+//name,age,city
+//two objects and create a function named display
+//to show the details
+public class Main {
+    public static void main(String[] args) {
+        //First object
+        Employee e1 = new Employee();
+        e1.name="Venkat";
+        e1.age=19;
+        e1.city="Hyderabad";
+        e1.display();
+        //Second object
+        Employee e2 = new Employee();
+        e2.name="Unknown";
+        e2.age=20;
+        e2.city="Indore";
+        e2.display();
     }
 }
